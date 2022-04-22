ab143a9893cd59068fb62bc5fad44a3e076f18e2
Rajaram AKR <rajaram.akr@gmail.com>
2022-04-22 10:57:32 +0530

    New modification tested

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit Test <code>src/App.js</code> and save to reload. New dev branch added. New modification tested.
+          Edit Test <code>src/App.js</code> and save to reload. New dev branch added. New modification tested. Dev o Master.
         </p>
         <a
           className="App-link"
